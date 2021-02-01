b71b5a1712c4e375aee8f0a0afc2e7045719dac5
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2021-02-01 23:16:13 +0100

    firefox: Hardware acceleration with WebRender

--- home-manager/profiles/programs/firefox/default.nix
+++ home-manager/profiles/programs/firefox/default.nix
@@ -11,9 +11,18 @@ with builtins;
       path = "zemg8lg7.default-1600854037501";
 
       settings = {
+        # Styling/Interface
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "browser.tabs.loadBookmarksInTabs" = true;
         "signon.rememberSignons" = false;
+
+        # Performance, overall
+        "extensions.pocket.enabled" = false;
+
+        # Performance, rendering
+        "gfx.webrender.compositor.force-enabled" = true;
+        "gfx.webrender.enabled" = true;
+        "layers.acceleration.force-enabled" = true;
       };
 
       userChrome = readFile ./userChrome.css;
