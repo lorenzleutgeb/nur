0cd9bd230cfa380cbd8a325646366a8d59dc6495
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2021-02-28 15:42:26 +0100

    Firefox configuration

--- home-manager/profiles/programs/firefox/default.nix
+++ home-manager/profiles/programs/firefox/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ super, ... }:
 
 with builtins;
 
@@ -14,15 +14,33 @@ with builtins;
         # Styling/Interface
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "browser.tabs.loadBookmarksInTabs" = true;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "browser.uidensity" = 1;
+        "browser.urlbar.oneOffSearches" = false;
+        "browser.urlbar.update2.oneOffsRefresh" = false;
         "signon.rememberSignons" = false;
 
         # Performance, overall
         "extensions.pocket.enabled" = false;
+        "extensions.pocket.oAuthConsumerKey" = "";
 
         # Performance, rendering
         "gfx.webrender.compositor.force-enabled" = true;
         "gfx.webrender.enabled" = true;
         "layers.acceleration.force-enabled" = true;
+
+        # Security
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_pbm" = true;
+
+        # Privacy
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.pbmode.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "privacy.trackingprotection.cryptomining.enabled" = true;
+
+        "identity.fxaccounts.account.device.name" = super.networking.hostName;
       };
 
       userChrome = readFile ./userChrome.css;
