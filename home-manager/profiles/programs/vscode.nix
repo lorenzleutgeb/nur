d5d0ea818725696753b8d06eb85cca69882b67ac
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2021-12-03 13:48:08 +0100

    hm: Add Visual Studio Code configuration

--- home-manager/profiles/programs/vscode.nix
+++ home-manager/profiles/programs/vscode.nix
@@ -2,6 +2,31 @@
   services.vscode-server.enable = true;
   programs.vscode = {
     enable = true;
-    #extensions = [ pkgs.vscode-extensions.vscodevim ];
+    userSettings = {
+      "editor.fontFamily" = "Fira Code, Consolas, monospace";
+      "editor.fontLigatures" = true;
+      "editor.guides.bracketPairs" = true;
+
+      "lean4.input.languages" = "lean4";
+
+      "update.channel" = "none";
+
+      # TODO: Get rid of hardcoded Tailscale IPs
+      "remote.SSH.remotePlatform"."100.85.40.10" = "linux";
+    };
+    extensions = with pkgs.vscode-extensions; [
+      arrterian.nix-env-selector
+      bbenoist.nix
+      haskell.haskell
+      jnoortheen.nix-ide
+      justusadam.language-haskell
+      matklad.rust-analyzer
+      vscodevim.vim
+
+      # Not packaged in Nixpkgs:
+      #bungcip.better-toml
+      #dandric.vscode-jq
+      #jq-syntax-highlighting.jq-syntax-highlighting
+    ];
   };
 }
