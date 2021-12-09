b96e944f310c23aa91d13b21872bb2852643eece
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2021-12-09 17:17:12 +0100

    hm/docker: Use pass as credential store

--- home-manager/profiles/docker.nix
+++ home-manager/profiles/docker.nix
@@ -1,9 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    docker-credential-helpers
+  ];
+
   # XDG Issue: https://github.com/moby/moby/issues/20693
+  #xdg.configFile."docker/config.json".text = builtins.toJSON {
   home.file.".docker/config.json".text = builtins.toJSON {
-    auths = { };
+    # To log in to a new registry:
+    #   1. `rm ~/.docker/config.json`
+    #   2. `docker login`
+    #   3. Make sure that `jq '.auths' < ~/.docker/config.json` matches what's below.
+    #   4. `rm ~/.docker/config.json`
+    #   5. nixos-rebuild
+    auths = {
+      "ghcr.io" = {};
+      "https://index.docker.io/v1/" = {};
+    };
     credHelpers = {
       "gcr.io" = "gcloud";
       "us.gcr.io" = "gcloud";
@@ -12,5 +26,7 @@
       "staging-k8s.gcr.io" = "gcloud";
       "marketplace.gcr.io" = "gcloud";
     };
+    # See https://docs.docker.com/engine/reference/commandline/login/#credentials-store
+    credsStore = "pass";
   };
 }
