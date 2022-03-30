2334f2da8d20e81a1f90c7c9a7d703427ab97be8
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2022-03-30 10:45:14 +0200

    hm/thunderbird: Note on date format
    After a hint from @filippodebortoli

--- home-manager/profiles/programs/thunderbird/default.nix
+++ home-manager/profiles/programs/thunderbird/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  # Until there's a module for Thunderbird, use Preferences / Config Editor
+  # intl.date_time.pattern_override.date_short = yyyy-MM-dd
   home = {
     file.".thunderbird/q3qx3d7w.default/chrome/userChrome.css".source =
       ./userChrome.css;
