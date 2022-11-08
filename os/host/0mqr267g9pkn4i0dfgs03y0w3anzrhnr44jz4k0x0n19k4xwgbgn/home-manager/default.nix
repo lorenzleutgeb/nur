e6d0057d556533f69c1bc40cf3f024f0f78aa4b7
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2022-11-08 19:26:50 +0100

    0mqr: Enable SPASS profile

--- os/host/0mqr267g9pkn4i0dfgs03y0w3anzrhnr44jz4k0x0n19k4xwgbgn/home-manager/default.nix
+++ os/host/0mqr267g9pkn4i0dfgs03y0w3anzrhnr44jz4k0x0n19k4xwgbgn/home-manager/default.nix
@@ -7,6 +7,7 @@ with builtins;
     ./autorandr
     ../../../../home-manager/profiles/development.nix
     ../../../../home-manager/profiles/gaming.nix
+    ../../../../home-manager/profiles/spass.nix
     ../../../../home-manager/profiles/services/v4lbridge.nix
   ];
 
