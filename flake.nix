796e2841df4fa82d5fb4d7c246db73b2938c62c0
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2023-08-10 18:56:58 +0200

    flake: update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       flake = false;
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
