b22041479b39caa1bfed1d16de1935f58cc91d75
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2023-08-14 22:06:58 +0200

    overlay: Add zoom

---
+++ overlay/zoom.nix
@@ -0,0 +1,13 @@
+# https://github.com/sebastiant/dotfiles/blob/367668f28ca6770493c78f4306338dcfbddbccff/programs/wayland-overlay.nix
+# ~/config/zoomus.conf: enableWaylandShare=true
+final: prev: {
+  zoom-us = prev.zoom-us.overrideAttrs (oldAttrs: rec {
+    postFixup =
+      oldAttrs.postFixup
+      + ''
+        mv $out/bin/{zoom,zoom-x11}
+        makeWrapper $out/bin/zoom-x11 $out/bin/zoom \
+          --unset XDG_SESSION_TYPE
+      '';
+  });
+}
