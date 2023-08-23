5571df5bc95bf92a6704bacdb0e19c0cc85a7821
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2023-08-23 09:58:44 +0200

    nix: Disable no-url-literals for ngipkgs

--- os/mixin/nix.nix
+++ os/mixin/nix.nix
@@ -21,13 +21,19 @@
     };
     extraOptions = ''
       allow-import-from-derivation = true
-      experimental-features = nix-command flakes no-url-literals repl-flake
+      experimental-features = nix-command flakes repl-flake
       builders-use-substitutes = true
       log-lines = 30
       max-silent-time = 600
       timeout = 7200
       #pure-eval = true
       #use-xdg-base-directories = true
+
+      # NOTE: Disabling URL literals is desired, but breaks
+      # evaluation of ngipkgs as of 2023-08-23. Bring it back
+      # once ngipkgs evaluates with it.
+      # See <https://github.com/ngi-nix/ngipkgs/issues/39>.
+      # experimental-features = no-url-literals
     '';
     gc.automatic = true;
   };
