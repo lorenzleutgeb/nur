8812b2dd6328ddafbe30afeedde9867c2a971ffa
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2023-09-27 13:33:11 +0200

    son: Add additional Cachix cache

--- hm/profiles/summer-of-nix.nix
+++ hm/profiles/summer-of-nix.nix
@@ -2,21 +2,23 @@
   pkgs,
   osConfig,
   ...
-}: let
-  cache = "mob";
-in {
+}: {
   home.packages = with pkgs; [alejandra espeak-classic mob];
 
-  systemd.user.services.cachix-watch-store = {
-    Unit = {
-      Description = "Cachix watching the store";
-      After = ["network-online.target"];
-    };
-    Service = {
-      Restart = "on-failure";
-      RestartSec = "1";
-      Environment = "PATH=${osConfig.nix.package}/bin";
-      ExecStart = "${pkgs.cachix}/bin/cachix watch-store ${cache}";
-    };
-  };
+  systemd.user.services = with builtins;
+    listToAttrs (map (cache: {
+      name = "cachix-watch-store@${cache}";
+      value = {
+        Unit = {
+          Description = "Cachix watching the store";
+          After = ["network-online.target"];
+        };
+        Service = {
+          Restart = "on-failure";
+          RestartSec = "1";
+          Environment = "PATH=${osConfig.nix.package}/bin";
+          ExecStart = "${pkgs.cachix}/bin/cachix watch-store ${cache}";
+        };
+      };
+    }) ["mob" "ngi"]);
 }
