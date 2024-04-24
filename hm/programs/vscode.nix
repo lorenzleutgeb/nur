c090f492625417e6da8b7936e2a1a18e2819fd5e
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-04-24 22:33:27 +0200

    vscode: Enable Java for ltex

--- hm/programs/vscode.nix
+++ hm/programs/vscode.nix
@@ -1,46 +1,46 @@
 {pkgs, ...}: {
   services.vscode-server.enable = true;
-  programs.vscode = {
-    enable = true;
-    userSettings = {
-      "editor.fontFamily" = "Fira Code, Consolas, monospace";
-      "editor.fontLigatures" = true;
-      "editor.guides.bracketPairs" = true;
+  programs = {
+    java.enable = true; # Some extensions, e.g. `valentjn.vscode-ltex` require Java.
 
-      "lean4.input.languages" = "lean4";
+    vscode = {
+      enable = true;
+      userSettings = {
+        "editor.fontFamily" = "Fira Code, Consolas, monospace";
+        "editor.fontLigatures" = true;
+        "editor.guides.bracketPairs" = true;
+        "lean4.input.languages" = "lean4";
+        "remote.SSH.remotePlatform"."0mqr.lorenz.hs.leutgeb.xyz" = "linux";
+        "rust-analyzer.updates.askBeforeDownload" = false;
+        "update.channel" = "none";
+      };
+      extensions = with pkgs.vscode-extensions; [
+        arrterian.nix-env-selector
+        bbenoist.nix
+        eamodio.gitlens
+        github.copilot
+        haskell.haskell
+        jnoortheen.nix-ide
+        justusadam.language-haskell
+        matklad.rust-analyzer
+        mkhl.direnv
+        ms-python.python
+        # See https://code.visualstudio.com/docs/cpp/config-linux
+        # See https://code.visualstudio.com/docs/cpp/config-wsl
+        ms-vscode.cpptools
+        #leanprover.lean4
+        vscodevim.vim
 
-      "update.channel" = "none";
-
-      "remote.SSH.remotePlatform"."0mqr.fluffy-ordinal.ts.net" = "linux";
-
-      "rust-analyzer.updates.askBeforeDownload" = false;
+        # Not packaged in Nixpkgs:
+        #bungcip.better-toml
+        #dandric.vscode-jq
+        #jq-syntax-highlighting.jq-syntax-highlighting
+        #ms-python.flake8
+        #ms-vscode.cmake-tools
+        #ms-vscode.cpptools-themes
+        #ms-vscode.cpptools-extension-pack
+        #TabNine.tabnine-vscode
+      ];
     };
-    extensions = with pkgs.vscode-extensions; [
-      arrterian.nix-env-selector
-      bbenoist.nix
-      eamodio.gitlens
-      github.copilot
-      haskell.haskell
-      jnoortheen.nix-ide
-      justusadam.language-haskell
-      matklad.rust-analyzer
-      mkhl.direnv
-      ms-python.python
-      # See https://code.visualstudio.com/docs/cpp/config-linux
-      # See https://code.visualstudio.com/docs/cpp/config-wsl
-      ms-vscode.cpptools
-      #leanprover.lean4
-      vscodevim.vim
-
-      # Not packaged in Nixpkgs:
-      #bungcip.better-toml
-      #dandric.vscode-jq
-      #jq-syntax-highlighting.jq-syntax-highlighting
-      #ms-python.flake8
-      #ms-vscode.cmake-tools
-      #ms-vscode.cpptools-themes
-      #ms-vscode.cpptools-extension-pack
-      #TabNine.tabnine-vscode
-    ];
   };
 }
---
+++ pkg/briss-ng.nix
@@ -0,0 +1,38 @@
+
+{
+  src = pkgs.fetchurl {
+    sha256 = "0ldh303r5063kd5y73hhkbd9v11c98aki8wjizmchzx2blwlipy7";
+  };
+
+
+}
+
+{ lib, stdenv, fetchurl, jre, makeWrapper }:
+
+stdenv.mkDerivation rec {
+  pname = "briss";
+  version = "2.0-alpha-5";
+  src = fetchurl {
+    url = "mirror://sourceforge/briss/briss-${version}.tar.gz";
+    url = "https://github.com/mbaeuerle/Briss-2.0/releases/download/v2${version}/Briss-2.0.zip";
+    sha256 = lib.fakeHash;
+  };
+
+  nativeBuildInputs = [makeWrapper];
+
+  installPhase = ''
+    mkdir -p "$out/bin";
+    mkdir -p "$out/share";
+    install -D -m444 -t "$out/share" *.jar
+    makeWrapper "${jre}/bin/java" "$out/bin/briss" --add-flags "-Xms128m -Xmx1024m -cp \"$out/share/\" -jar \"$out/share/briss-${version}.jar\""
+  '';
+
+  meta = {
+    homepage = "https://github.com/mbaeuerle/Briss-2.0";
+    description = "Java application for cropping PDF files";
+    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.unix;
+    mainProgram = "briss";
+  };
+}
