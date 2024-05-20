d87f90c3c41b1c5f434282be93775027fe1473f5
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-05-20 21:13:52 +0200

    hm/scripts: Try to get rid of `osConfig`

--- hm/scripts/default.nix
+++ hm/scripts/default.nix
@@ -1,27 +1,30 @@
 {
   lib,
   pkgs,
-  osConfig,
+  osConfig ? null,
+  config,
   ...
-}:
-with builtins; {
-  home.packages = let
-    nix-nom = pkgs.writeShellApplication {
-      name = "nix-nom";
-      text = ''
-        set -euo pipefail
-        if [ "$1" = "build" ] || [ "$1" = "shell" ] || [ "$1" = "develop" ]
-        then
-          ${pkgs.nix-output-monitor}/bin/nom "''${@:1}"
-        else
-          ${osConfig.nix.package}/bin/nix "''${@:1}"
-        fi
-      '';
-    };
-  in
-    (map (x: (pkgs.writeScriptBin x (readFile (./. + "/${x}"))))
-      (attrNames (readDir ./.)))
-    ++ [
-      nix-nom
-    ];
-}
+} @ args: let
+  nix = (args.osConfig or config).nix.package;
+in
+  with builtins; {
+    home.packages = let
+      nix-nom = pkgs.writeShellApplication {
+        name = "nix-nom";
+        text = ''
+          set -euo pipefail
+          if [ "$1" = "build" ] || [ "$1" = "shell" ] || [ "$1" = "develop" ]
+          then
+            ${lib.getExe pkgs.nix-output-monitor} "''${@:1}"
+          else
+            ${lib.getExe nix} "''${@:1}"
+          fi
+        '';
+      };
+    in
+      (map (x: (pkgs.writeScriptBin x (readFile (./. + "/${x}"))))
+        (attrNames (readDir ./.)))
+      ++ [
+        nix-nom
+      ];
+  }
