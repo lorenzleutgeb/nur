04eb8d0ddbf4aa777c9ad674f7cd70fb4642fe3f
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-06-10 22:47:02 +0200

    hardware/intel-graphics: Simplify

--- os/mixin/intel-graphics.nix
+++ os/mixin/intel-graphics.nix
@@ -1,22 +1,23 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.hardware.nixosModules.common-gpu-intel
+  ];
+
   environment.systemPackages = with pkgs; [
     intel-gpu-tools
     libva-utils
-    #vdpauinfo
+    vdpauinfo
   ];
 
   hardware = {
     opengl = {
-      # NOTE: Couldn't get VDPAU via VAAPI to work. Probably don't need it.
       enable = true;
       driSupport = true;
       driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        intel-media-driver
-        vaapiIntel
-        #libvdpau
-        #libvdpau-va-gl
-      ];
     };
   };
 }
