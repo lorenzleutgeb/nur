f326a5239cc1faa3e149a7fb67676aa5591fb1bb
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-06-13 01:33:37 +0200

    dns: Hack a status page

---
+++ os/host/nc/knot-status.sh
@@ -0,0 +1,107 @@
+#! /bin/sh
+set -eu
+
+HTML="${1:-}"
+
+MD="$(mktemp --suffix .md)"
+
+printf "$MD\n"
+
+function md {
+	if [ "$HTML" != "" ]
+	then
+		printf "${@}" >> $MD
+	fi
+}
+
+md "%s\n" "---"
+md "title: DNS Status\n"
+md "mainfont: sans-serif\n"
+md "monofont: \"'JetBrains Mono', 'Fira Code', 'Source Code Pro', 'Cascadia Code', monospace\"\n"
+md "date: %s\n" "$(date --iso-8601=seconds)"
+md "author: '[Lorenz Leutgeb](mailto:lorenz@leutgeb.xyz?subject=DNS%%20Status)'\n"
+md "abstract: 'This page shows up-to-date authoritative information and whether it has propagated to secondaries.'\n"
+md "description: 'DNS Status shows up-to-date authoritative information and whether it has propagated to secondaries.'\n"
+md "abstract-title: ''\n"
+md "subtitle: Propagation at a Glance\n"
+md "lang: en\n"
+md "footer: en\n"
+md "backgroundcolor: black\n"
+md "fontcolor: white\n"
+md "linkcolor: '#0088ff'\n"
+md "%s\n" "---"
+
+md "\n## Authoritative Servers\n\n"
+
+md "<details><summary><h3 style=\"display:inline;\">Primary</h3></summary>\n\n"
+
+DIG_ARGS="+multiline +norecurse +nostats +noquestion +nocomments +noadditional +noauthority +nocmd"
+
+KNOT_CONF="/etc/knot/knot.conf"
+
+DOMAINS="$(yq -r '.zone[].domain' $KNOT_CONF)"
+PRIMARY="$(yq -r '.server.listen[0]' $KNOT_CONF)"
+
+PRIMARY_RESULT="$(mktemp -t resolve-$(date --iso-8601)-XXXX --suffix=-primary)"
+dig $DIG_ARGS "@$PRIMARY" SOA ${DOMAINS} > "$PRIMARY_RESULT"
+
+dig +noall +answer "@$PRIMARY" SOA ${DOMAINS} | sed 's/\t\+/  /g'
+
+md "\`\`\`\n%s\n\`\`\`\n\n" "$(cat "$PRIMARY_RESULT" | sed 's/\.\t\t/. \\\n/;s/\t\t\t\t/  /;s/  )/)\n/')"
+
+SECONDARIES="$(dig +noall +answer +short "@$PRIMARY" ns $DOMAINS | grep -v "$(yq -r '"\\(" + ([.zone[].domain] | join("\\|")) + "\\)"' $KNOT_CONF)" | jq -Rr 'split(".") | .[0:-1] | reverse | join(".")' | sort -u)"
+
+EXIT="0"
+
+md "</details>\n\n"
+md "### Secondaries\n"
+
+md "<table><thead><tr><th>Server</th><th>Status</th></thead><tbody>\n"
+
+DIFF_FILE="$(mktemp)"
+
+for SECONDARY in ${SECONDARIES}
+do
+	md "<tr><td>%s</td>" "$SECONDARY"
+	printf "%s: " "$SECONDARY"
+	SECONDARY_SANE="$(printf "$SECONDARY\n" | jq -Rr 'split(".") | reverse | join(".")')"
+	if ! \
+		dig $DIG_ARGS "@$SECONDARY_SANE" SOA ${DOMAINS[@]} 2>&1 | \
+		diff -U6 --label "primary" "$PRIMARY_RESULT" --label "secondary/$SECONDARY" - \
+		> "$DIFF_FILE"
+	then
+		EXIT="1"
+		printf "FAIL\n"
+		md "<td>\n"
+		md "\`\`\`diff\n%s\`\`\`\n" "$(cat $DIFF_FILE)"
+		md "</td>\n"
+	else
+		printf "PASS\n"
+		md "<td>✅</td>\n"
+	fi
+	md "</tr>\n" "$SECONDARY"
+done
+
+md "\n</tbody></table>\n\n"
+
+md "## Checkers\n\n<dl>\n\n"
+
+for DOMAIN in ${DOMAINS}
+do
+	md "<dt>%s</dt><dd><ul style=\"margin-top:0;margin-bottom:16px;\">" "$DOMAIN"
+	md "<li style=\"line-height: 200%%;\"><a target=\"_blank\" href=\"https://www.whatsmydns.net/#SOA/%s/%s\">DNS Propagation</a></li>\n" "$DOMAIN" "$(dig +noall +answer +short "@$PRIMARY" SOA "$DOMAIN" | sed 's/\. / /g;s/ /%20/g')"
+	md "<li style=\"line-height: 200%%;\"><a target=\"_blank\" href=\"https://mxtoolbox.com/Supertool.aspx?action=dns:%s\">MXToolBox</a></li>\n" "$DOMAIN"
+	md "<li style=\"line-height: 200%%;\"><a target=\"_blank\" href=\"https://dnssec-analyzer.verisignlabs.com/%s\">Verisign DNSSEC Analyzer</a></li>\n" "$DOMAIN"
+	md "<li style=\"line-height: 200%%;\"><a target=\"_blank\" href=\"https://dnsviz.net/d/%s\">DNSViz</a></li>\n" "$DOMAIN"
+	md "</ul></dd>"
+done
+
+md "\n</dl>\n\n"
+
+
+if [ "$HTML" != "" ]
+then
+	pandoc -s $MD -o $HTML
+fi
+
+exit "$EXIT"
--- os/host/nc/knot.nix
+++ os/host/nc/knot.nix
@@ -21,6 +21,8 @@
     '';
 
   keys = ["acme" "he"];
+
+  path = ./. + "/zone";
 in {
   networking.firewall = let
     dns = [53];
@@ -39,165 +41,198 @@ in {
     })
     keys);
 
-  services.knot = {
-    enable = true;
+  services = {
+    caddy.virtualHosts."dns.leutgeb.xyz".extraConfig = ''
+      root /zone/* ${path}
+      root * /var/www/dns.leutgeb.xyz
+      encode zstd gzip
+      file_server
+    '';
 
-    keyFiles = map (key: config.sops.secrets."knot/${key}".path) keys;
+    knot = {
+      enable = true;
 
-    settings = {
-      server = {
-        listen = [
-          "5.45.105.177" # netcup
-          "2a03:4000:6:10ea:54b5:3dff:fe79:b5b9" # netcup
-          "100.96.0.2" # tailnet
-          "fd7a:115c:a1e0:e00::2" # tailnet
+      keyFiles = map (key: config.sops.secrets."knot/${key}".path) keys;
+
+      settings = {
+        server = {
+          listen = [
+            "5.45.105.177" # netcup
+            "2a03:4000:6:10ea:54b5:3dff:fe79:b5b9" # netcup
+            "100.96.0.2" # tailnet
+            "fd7a:115c:a1e0:e00::2" # tailnet
+          ];
+
+          automatic-acl = true;
+        };
+
+        remote = [
+          {
+            id = "slave.dns.he.net";
+            address = ["2001:470:600::2" "216.218.133.2"];
+
+            # NOTE: Sadly does not work.
+            # ACL, allowed, action query, remote …, key 1fc…zzm.
+            # TSIG, key '1fc…zzm.', verification failed 'BADSIG'
+            #key = "1FCTMVl4ITuOsxyYmWFymxz8VhXMzzZM";
+          }
+          {
+            id = "ns1.he.net";
+            address = ["2001:470:100::2" "216.218.130.2"];
+          }
+          {
+            id = "ns2.afraid.org";
+            address = [
+              "69.65.50.192"
+
+              # NOTE: afraid.org seems to only pull and allow our A, but not AAAA records.
+              #"2001:1850:1:5:800::6b"
+            ];
+          }
+          {
+            id = "puck.nether.net";
+            address = [
+              "2602:fe55:5::5"
+
+              # NOTE: Only our IPv6 is allowlisted on their end.
+              #"204.42.254.5"
+            ];
+          }
+          {
+            id = "1984.is";
+            address = "93.95.224.6";
+          }
+          {
+            id = "quad9";
+            address = ["2620:fe::fe" "2620:fe::9" "9.9.9.9" "149.112.112.112"];
+          }
         ];
 
-        automatic-acl = true;
+        remotes = [
+          {
+            id = "notify";
+            remote = [
+              "ns1.he.net"
+              "ns2.afraid.org"
+              "puck.nether.net"
+              "1984.is"
+            ];
+          }
+          {
+            id = "transfer";
+            remote = [
+              "slave.dns.he.net"
+              "ns2.afraid.org"
+              "puck.nether.net"
+              "1984.is"
+            ];
+          }
+        ];
+
+        log = [
+          {
+            target = "syslog";
+            any = "debug";
+          }
+        ];
+
+        acl = [
+          {
+            id = "transfer";
+            action = [
+              "query"
+              "transfer"
+            ];
+            remote = "transfer";
+          }
+          {
+            id = "acme";
+            action = "update";
+            key = "acme";
+          }
+          {
+            id = "tailnet";
+            action = [
+              "transfer"
+              "update"
+            ];
+            address = config.services.headscale.settings.ip_prefixes;
+          }
+        ];
+
+        mod-rrl = [
+          {
+            id = "default";
+            rate-limit = 500;
+            slip = 2;
+          }
+        ];
+
+        mod-dnsproxy = [
+          {
+            id = "default";
+            remote = "quad9";
+            fallback = true;
+          }
+        ];
+
+        template = [
+          {
+            id = "default";
+            semantic-checks = "on";
+            global-module = [
+              "mod-rrl/default"
+              #"mod-dnsproxy/default"
+            ];
+            zonefile-load = "difference-no-serial";
+            zonefile-sync = "-1";
+            journal-content = "all";
+          }
+          {
+            id = "primary";
+            notify = "notify";
+            acl = ["transfer" "tailnet"];
+            dnssec-signing = true;
+          }
+        ];
+
+        zone = let
+          primary = domain: {
+            inherit domain;
+            file = "${path}/${domain}.zone";
+            template = "primary";
+          };
+        in [
+          (primary "falsum.org")
+          (primary "leutgeb.xyz")
+          (primary "leutgeb.wien")
+        ];
       };
-
-      remote = [
-        {
-          id = "slave.dns.he.net";
-          address = ["2001:470:600::2" "216.218.133.2"];
-
-          # NOTE: Sadly does not work.
-          # ACL, allowed, action query, remote …, key 1fc…zzm.
-          # TSIG, key '1fc…zzm.', verification failed 'BADSIG'
-          #key = "1FCTMVl4ITuOsxyYmWFymxz8VhXMzzZM";
-        }
-        {
-          id = "ns1.he.net";
-          address = ["2001:470:100::2" "216.218.130.2"];
-        }
-        {
-          id = "ns2.afraid.org";
-          address = [
-            "69.65.50.192"
-
-            # NOTE: afraid.org seems to only pull and allow our A, but not AAAA records.
-            #"2001:1850:1:5:800::6b"
-          ];
-        }
-        {
-          id = "puck.nether.net";
-          address = [
-            "2602:fe55:5::5"
-
-            # NOTE: Only our IPv6 is allowlisted on their end.
-            #"204.42.254.5"
-          ];
-        }
-        {
-          id = "1984.is";
-          address = "93.95.224.6";
-        }
-        {
-          id = "quad9";
-          address = ["2620:fe::fe" "2620:fe::9" "9.9.9.9" "149.112.112.112"];
-        }
+    };
+  };
+  systemd = {
+    services.knot-status = {
+      serviceConfig.Type = "oneshot";
+      script = builtins.readFile ./knot-status.sh;
+      scriptArgs = "/var/www/dns.leutgeb.xyz/index.html";
+      wants = ["knot.service"];
+      requires = ["knot.service"];
+      path = with pkgs; [
+        dnsutils # for dig
+        yq
+        jq
+        coreutils
+        diffutils
+        pandoc
       ];
-
-      remotes = [
-        {
-          id = "notify";
-          remote = [
-            "ns1.he.net"
-            "ns2.afraid.org"
-            "puck.nether.net"
-            "1984.is"
-          ];
-        }
-        {
-          id = "transfer";
-          remote = [
-            "slave.dns.he.net"
-            "ns2.afraid.org"
-            "puck.nether.net"
-            "1984.is"
-          ];
-        }
-      ];
-
-      log = [
-        {
-          target = "syslog";
-          any = "debug";
-        }
-      ];
-
-      acl = [
-        {
-          id = "transfer";
-          action = [
-            "query"
-            "transfer"
-          ];
-          remote = "transfer";
-        }
-        {
-          id = "acme";
-          action = "update";
-          key = "acme";
-        }
-        {
-          id = "tailnet";
-          action = [
-            "transfer"
-            "update"
-          ];
-          address = config.services.headscale.settings.ip_prefixes;
-        }
-      ];
-
-      mod-rrl = [
-        {
-          id = "default";
-          rate-limit = 500;
-          slip = 2;
-        }
-      ];
-
-      mod-dnsproxy = [
-        {
-          id = "default";
-          remote = "quad9";
-          fallback = true;
-        }
-      ];
-
-      template = [
-        {
-          id = "default";
-          semantic-checks = "on";
-          global-module = [
-            "mod-rrl/default"
-            #"mod-dnsproxy/default"
-          ];
-          zonefile-load = "difference-no-serial";
-          zonefile-sync = "-1";
-          journal-content = "all";
-        }
-        {
-          id = "primary";
-          notify = "notify";
-          acl = ["transfer" "tailnet"];
-          dnssec-signing = true;
-        }
-      ];
-
-      zone = let
-        path = ./. + "/zone";
-        primary = domain: {
-          inherit domain;
-          file = "${path}/${domain}.zone";
-          template = "primary";
-        };
-      in [
-        (primary "falsum.org")
-        (primary "leutgeb.xyz")
-        (primary "leutgeb.wien")
-      ];
+    };
+    timers.knot-status = {
+      timerConfig = {
+        OnCalendar = "*:0/30";
+        RandomizedDelaySec = "5min";
+        Unit = "knot-status.service";
+      };
+      wantedBy = ["timers.target"];
     };
   };
 }
