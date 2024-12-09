1a7360f70e345a22a1be26238d5eec7be10bec3f
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-12-09 13:40:41 +0100

    hm/radicle: Function for seeds

--- hm/services/radicle.nix
+++ hm/services/radicle.nix
@@ -11,21 +11,45 @@ in {
       rad.browser.enable = true;
     };
     settings = {
-      preferredSeeds =
-        [
-          "z6MkjDYUKMUeY58Vtr8dGJrHRvnTfjKWVGCBYJDVTHXsXzm5@seed.radicle.at:8776"
-          "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@seed.radicle.garden:8776"
-          "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@ash.radicle.garden:8776"
-          "z6MktrU3HB5hkin6M5PLbHn6GYD25yJH22zkJe9SqezmUcGm@seed.cloudhead.io:8776"
-          "z6MkfXa53s1ZSFy8rktvyXt5ADCojnxvjAoQpzajaXyLqG5n@radicle.liw.fi:8776"
-        ]
-        ++ [
-          "z6MkocYY4dgMjo2YeUEwQ4BP4AotL7MyovzJCPiEuzkjg127@${
+      preferredSeeds = let
+        seed = {
+          key,
+          hostname,
+          port ? 8776,
+        }: "${key}@${hostname}:${builtins.toString port}";
+      in [
+        (seed {
+          key = "z6MkjDYUKMUeY58Vtr8dGJrHRvnTfjKWVGCBYJDVTHXsXzm5";
+          hostname = "seed.radicle.at";
+        })
+        (seed {
+          key = "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7";
+          hostname = "seed.radicle.garden";
+        })
+        (seed {
+          key = "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo";
+          hostname = "ash.radicle.garden";
+        })
+        (seed {
+          key = "z6MktrU3HB5hkin6M5PLbHn6GYD25yJH22zkJe9SqezmUcGm";
+          hostname = "seed.cloudhead.io";
+        })
+        (seed {
+          key = "z6MkfXa53s1ZSFy8rktvyXt5ADCojnxvjAoQpzajaXyLqG5n";
+          hostname = "radicle.liw.fi";
+        })
+        (seed {
+          key = "z6MksmpU5b1dS7oaqF2bHXhQi1DWy2hB7Mh9CuN7y1DN6QSz";
+          hostname = "seed.radicle.xyz";
+        })
+        (seed {
+          key = "z6MkocYY4dgMjo2YeUEwQ4BP4AotL7MyovzJCPiEuzkjg127";
+          hostname =
             if tor.enable
             then "q37hn7rhcbqiirapcx5bkgxzue5oqijdmjv55anfht2ne5hxxjxyhhyd.onion"
-            else "seed.leutgeb.xyz"
-          }:8776"
-        ];
+            else "seed.leutgeb.xyz";
+        })
+      ];
       node.onion = lib.mkIf tor.enable {
         mode = "proxy";
         address = with tor.socksListenAddress; "${addr}:${builtins.toString port}";
