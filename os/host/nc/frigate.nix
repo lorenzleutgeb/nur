93179cf6cb26b30f9fb125c7f5c5a406c0d26bc0
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-01-16 10:49:47 +0100

    nc/frigate: Attempt to fix

--- os/host/nc/frigate.nix
+++ os/host/nc/frigate.nix
@@ -1,5 +1,6 @@
 {config, ...}: let
   hostname = "frigate.leutgeb.xyz";
+  port = 7070;
 in {
   services = {
     frigate = {
@@ -8,11 +9,17 @@ in {
       settings = {
         mqtt.enabled = false;
 
+        tls.enabled = false;
+
         record = {
           enabled = true;
           retain = {
-            days = 2;
+            days = 7;
             mode = "all";
+          };
+          events.retain = {
+            default = 30;
+            mode = "motion";
           };
         };
 
@@ -20,18 +27,33 @@ in {
           ffmpeg.inputs = [
             {
               path = "rtsp://ghwunv0004:{FRIGATE_RTSP_PASSWORD}@192.168.178.57/mpeg/media.amp";
-              input_args = "preset-rtsp-restream";
+              #input_args = "preset-rtsp-restream";
               roles = ["record"];
             }
           ];
+        };
+
+        #auth.reset_admin_password = true;
+        /*
+        detectors."ov_0" = {
+          type = "openvino";
+          device = "CPU";
+        };
+        */
+
+        timestamp_style = {
+          position = "tl";
+          format = "%Y-%m-%dT%H:%M:%S%z";
         };
       };
     };
     nginx = {
       enable = true;
-      defaultHTTPListenPort = 7070;
+      defaultHTTPListenPort = port;
     };
   };
+
+  networking.firewall.allowedTCPPorts = [port];
 
   sops.secrets.frigate = {
     sopsFile = ./sops/frigate.env;
