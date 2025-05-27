ce01a291339d3cb38f26b735355d624f3d508d40
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-05-27 20:39:06 +0200

    hm/jj: Expand configuration

--- hm/programs/jj.nix
+++ hm/programs/jj.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.jujutsu = {
     enable = true;
     settings = {
@@ -8,8 +12,49 @@
         name = userName;
         email = userEmail;
       };
+
+      core = {
+        fsmonitor = "watchman";
+        watchman.register-snapshot-trigger = true;
+      };
+      revset-aliases = {
+        "private()" = "description(glob:'wip:*') | description(glob:'private:*') | description(glob:'WIP:*') | description(glob:'PRIVATE:*') | conflicts() | (empty() ~ merges())";
+      };
+
+      aliases = {
+        dlog = ["log" "-r"];
+        l = ["log" "-r" "(trunk()..@):: | (trunk()..@)-"];
+        tug = [
+          "bookmark"
+          "move"
+          "--from"
+          "closest_bookmark(@)"
+          "--to"
+          "closest_pushable(@)"
+        ];
+      };
+
+      fix.tools.rustfmt = {
+        command = ["rustfmt" "--emit" "stdout"];
+        patterns = ["glob:'**/*.rs'"];
+      };
+
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = "~/.ssh/id_ed25519.pub";
+      };
     };
   };
 
-  home.shellAliases.j = "jj";
+  home = {
+    packages = with pkgs; [
+      jjui
+    ];
+    shellAliases = {
+      j = "jj";
+      jui = "jjui";
+      jinit = "jj git init --colocate";
+    };
+  };
 }
