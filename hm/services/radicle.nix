ec4743b428fb1fbf6b7670d27732e222487dbfd4
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-05-27 20:41:33 +0200

    hm/radicle: Remove preferred seeds

--- hm/services/radicle.nix
+++ hm/services/radicle.nix
@@ -35,45 +35,48 @@ in {
       web-rad.enable = false;
       rad.browser.enable = false;
     };
-    settings.node = {
-      connect = let
-        connectAddr = {
-          nid,
-          addr,
-          port ? 8776,
-        }: "${nid}@${addr}:${toString port}";
-      in
-        [
-          (connectAddr {
-            nid = "z6MksmpU5b1dS7oaqF2bHXhQi1DWy2hB7Mh9CuN7y1DN6QSz";
-            addr = "seed.radicle.xyz";
-          })
-        ]
-        ++ (
-          let
-            nid = "z6MkocYY4dgMjo2YeUEwQ4BP4AotL7MyovzJCPiEuzkjg127";
-          in
-            (optional tor.enable (connectAddr {
-              inherit nid;
-              addr = "q37hn7rhcbqiirapcx5bkgxzue5oqijdmjv55anfht2ne5hxxjxyhhyd.onion";
-            }))
-            ++ (optional ygg.enable (connectAddr {
-              inherit nid;
-              addr = "[202:f094:502b:1b03:9e0:2c3d:bc8b:428b]";
-            }))
-            ++ (optional (!ygg.enable && !tor.enable) (connectAddr {
-              inherit nid;
-              addr = "node.radicle.lorenz.leutgeb.xyz";
-            }))
-        );
-      onion = mkIf tor.enable {
-        mode = "proxy";
-        address = "${tor.socksListenAddress.addr}:${toString tor.socksListenAddress.port}";
+    settings = {
+      preferredSeeds = [];
+      node = {
+        connect = let
+          connectAddr = {
+            nid,
+            addr,
+            port ? 8776,
+          }: "${nid}@${addr}:${toString port}";
+        in
+          [
+            (connectAddr {
+              nid = "z6MksmpU5b1dS7oaqF2bHXhQi1DWy2hB7Mh9CuN7y1DN6QSz";
+              addr = "seed.radicle.xyz";
+            })
+          ]
+          ++ (
+            let
+              nid = "z6MkocYY4dgMjo2YeUEwQ4BP4AotL7MyovzJCPiEuzkjg127";
+            in
+              (optional tor.enable (connectAddr {
+                inherit nid;
+                addr = "q37hn7rhcbqiirapcx5bkgxzue5oqijdmjv55anfht2ne5hxxjxyhhyd.onion";
+              }))
+              ++ (optional ygg.enable (connectAddr {
+                inherit nid;
+                addr = "[202:f094:502b:1b03:9e0:2c3d:bc8b:428b]";
+              }))
+              ++ (optional (!ygg.enable && !tor.enable) (connectAddr {
+                inherit nid;
+                addr = "node.radicle.lorenz.leutgeb.xyz";
+              }))
+          );
+        onion = mkIf tor.enable {
+          mode = "proxy";
+          address = "${tor.socksListenAddress.addr}:${toString tor.socksListenAddress.port}";
+        };
+        externalAddresses =
+          (optional tor.enable tor.address) ++ (optional ygg.enable ygg.address);
+        listen =
+          (optional tor.enable "[::1]:${toString port}") ++ (optional ygg.enable ygg.address);
       };
-      externalAddresses =
-        (optional tor.enable tor.address) ++ (optional ygg.enable ygg.address);
-      listen =
-        (optional tor.enable "[::1]:${toString port}") ++ (optional ygg.enable ygg.address);
     };
   };
   services.radicle.node = {
