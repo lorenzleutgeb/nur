7db19ed3c60107c6b556da22708c0fd4eda410e5
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-06-17 22:32:01 +0200

    radicle: Remove useless SSH configurations

--- hm/profiles/radicle.nix
+++ hm/profiles/radicle.nix
@@ -1,15 +1,4 @@
 {pkgs, ...}: {
-  programs.ssh.matchBlocks = {
-    "seed.radicle.garden" = {
-      host = "seed.radicle.garden";
-      user = "seed";
-    };
-    "ash.radicle.garden" = {
-      host = "ash.radicle.garden";
-      user = "root";
-    };
-  };
-
   home.packages = with pkgs; [
     attic-client
     openssl
