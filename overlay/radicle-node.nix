2d0d6e2d36b31edc5b48088b9f7c37bb4f68cf51
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-10-16 22:23:45 +0200

    radicle: Update cutting edge

--- overlay/radicle-node.nix
+++ overlay/radicle-node.nix
@@ -9,8 +9,8 @@ in {
 
     src = final.fetchgit {
       inherit (prevAttrs.src) url;
-      rev = "f953c650e83bb7c94c38a3b4f940081e60a61320";
-      hash = "sha256-FzRPz47EtDAcmXAcRrgCREzGsnJ2R3ZO/i1n0HJYe6Y=";
+      rev = "615e37652285c6f06dc5bc28c8a13d4656f95104";
+      hash = "sha256-Kaam2qvoQn1wp1EYwm/nF0KXs52AUQDEkN01KMyBEY0=";
       leaveDotGit = true;
       postFetch = ''
         git -C $out rev-parse HEAD > $out/.git_head
@@ -21,7 +21,7 @@ in {
 
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit (finalAttrs) src;
-      hash = "sha256-iFWNGRXxt972qtSpt5y2vgqf3cys59G/qfgEO/BQbNk=";
+      hash = "sha256-OVHkufdivsCMW9AVTtXpmBQkFOPbgusqGQLaxX3TxAU=";
     };
 
     doCheck = false;
