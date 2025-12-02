4704060d28e0e879018d671afe005efae9e3a22a
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-12-03 00:05:19 +0100

    wsl/fstab: Disable fscking `C:\`

--- os/host/wsl/default.nix
+++ os/host/wsl/default.nix
@@ -86,11 +86,10 @@ in {
   fileSystems."/mnt/c" = {
     device = "C:";
     fsType = "drvfs";
+    noCheck = true;
     options = [
       "uid=${builtins.toString config.users.users.lorenz.uid}"
       "gid=${builtins.toString config.users.groups.${config.users.users.lorenz.group}.gid}"
-      #"x-systemd.automount"
-      #"x-systemd.mount-timeout=5s"
     ];
   };
 
